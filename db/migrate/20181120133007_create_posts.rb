7ba80950a8418e36fd971b432954baca21dd66a8
Nithesh Koneswaran <nk00374@surrey.ac.uk>
2018-11-20 14:41:13 +0000

    Added Posts controller + created a post table through a migration

--- db/migrate/20181120133007_create_posts.rb
+++ db/migrate/20181120133007_create_posts.rb
@@ -1,15 +1,15 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title null: false
-      t.text :description null: false
-      t.string :category null: false
-      t.string :price null: false
-      t.string :address_one :varchar(120) null: false
-      t.string :address_two :varchar(120)
-      t.string :address_three :varchar(120)
-      t.string :city :varchar(100) null: false
-      t.string :postal_code :varchar(16) null: false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :category, null: false
+      t.string :price, null: false
+      t.string :address_one, :length=>120, null: false
+      t.string :address_two, :length=>120
+      t.string :address_three, :length=>120
+      t.string :city, :length=>100, null: false
+      t.string :postal_code, :length=>16, null: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181118234806) do
+ActiveRecord::Schema.define(version: 20181120133007) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",         null: false
+    t.text     "description",   null: false
+    t.string   "category",      null: false
+    t.string   "price",         null: false
+    t.string   "address_one",   null: false
+    t.string   "address_two"
+    t.string   "address_three"
+    t.string   "city",          null: false
+    t.string   "postal_code",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
