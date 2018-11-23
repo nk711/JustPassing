86fe9e27e4d08b772f98151ccc0f542b512ede1d
nk00374 <nk00374@surrey.ac.uk>
2018-11-23 18:42:35 +0000

    Added User ID into post table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123181425) do
+ActiveRecord::Schema.define(version: 20181123184153) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",                           null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20181123181425) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "phone_number",  default: "",      null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
