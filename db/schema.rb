d01eb5142800d135c6fb06e5d48f951659340592
nk00374 <nk00374@surrey.ac.uk>
2018-11-23 18:59:40 +0000

    Added a new model category which will be used to categorise posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123185519) do
+ActiveRecord::Schema.define(version: 20181123185848) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",                      null: false
